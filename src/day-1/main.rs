a383ce63f7e110933606a3326fd9c033a290c57e
Isaac <isaac@grphcrtv.com>
2023-12-02 08:59:13 +1100

    day 1 part 2

--- src/day-1/main.rs
+++ src/day-1/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let input = std::fs::read("src/day-1/input.txt").expect("failed to open file");
+    let mut input = std::fs::read("src/day-1/input.txt").expect("failed to open file");
 
-    let sum = part_one(&input);
+    let sum = part_one(&mut input);
 
-    println!("Part one - {}", sum)
+    println!("Part one - {}", sum);
+
+    let sum_two = part_two(&input);
+
+    println!("Part two - {}", sum_two)
 }
 
 fn part_one(input: &mut Vec<u8>) -> u32 {
@@ -13,7 +17,7 @@ fn part_one(input: &mut Vec<u8>) -> u32 {
     let mut sum = 0;
 
     for (k, i) in input.into_iter().enumerate() {
-        let ch = char::from(i);
+        let ch = char::from(*i);
 
         match ch {
             '0'..='9' => {
@@ -38,26 +42,55 @@ fn part_two(input: &Vec<u8>) -> u32 {
 
     let mut buf = Vec::new();
     let mut sum = 0;
-    let mut word_buffer = String::from("");
 
     for (k, i) in input.into_iter().enumerate() {
-        let ch = char::from(i);
+        let ch = char::from(*i);
 
         match ch {
             '0'..='9' => {
                 buf.push(ch.to_digit(10).unwrap());
             }
-            _ => {
-                let peek = input[i + 1];
-
-                word_buffer.push(ch);
-                // "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-                if word_buffer == "one" {}
+            'o' => {
+                if k + 2 <= len && input[k..k + 3] == *"one".as_bytes() {
+                    buf.push(1)
+                }
             }
+            't' => {
+                if k + 2 <= len && input[k..k + 3] == *"two".as_bytes() {
+                    buf.push(2)
+                } else if k + 5 <= len && input[k..k + 5] == *"three".as_bytes() {
+                    buf.push(3)
+                }
+            }
+            'f' => {
+                if k + 4 <= len && input[k..k + 4] == *"four".as_bytes() {
+                    buf.push(4)
+                } else if k + 3 <= len && input[k..k + 4] == *"five".as_bytes() {
+                    buf.push(5)
+                }
+            }
+            's' => {
+                if k + 3 <= len && input[k..k + 3] == *"six".as_bytes() {
+                    buf.push(6)
+                } else if k + 5 <= len && input[k..k + 5] == *"seven".as_bytes() {
+                    buf.push(7)
+                }
+            }
+            'e' => {
+                if k + 5 <= len && input[k..k + 5] == *"eight".as_bytes() {
+                    buf.push(8)
+                }
+            }
+            'n' => {
+                if k + 4 <= len && input[k..k + 4] == *"nine".as_bytes() {
+                    buf.push(9)
+                }
+            }
+            _ => {}
         }
 
-        if ch == '\n' || k == len {
-            sum += format!("{}{}", buf[0], buf[buf.len() - 1])
+        if (ch == '\n' || k == len - 1) && buf.len() > 0 {
+            sum += format!("{}{}", buf[0], buf.last().unwrap())
                 .parse::<u32>()
                 .unwrap();
             buf.clear();
